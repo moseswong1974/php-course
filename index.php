8502c34da10f64a8936a174102c5aa0e7d71ce49
Wong <cheewai.wong@thermofisher.com>
2019-12-26 09:24:50 +0800

    add foreach loop

--- index.php
+++ index.php
@@ -542,7 +542,17 @@ echo $pageContents;
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
     echo "<h1>Welcome to ".$name."!</h1>";
-	echo "<h2>You have $".$credit."</h2>";
+	echo "<h2>You have $".$credit." in your wallet.</h2>";
+	
+		$products['Computer']=750;
+		$products['Car']=15000;
+		$products['iphone']=1000;
+		$products['Toaster']=75;
+		
+		foreach($products as $key => $value){
+			echo "<p>The ".$key." costs ".$value."</p>";
+		}
+		
     ?>
   </body>
 </html>
