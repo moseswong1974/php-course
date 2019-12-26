3770ef81dce2ae319366c2d167bcdf613ba29226
Wong <cheewai.wong@thermofisher.com>
2019-12-26 09:56:34 +0800

    add tax calc function

--- index.php
+++ index.php
@@ -539,10 +539,15 @@ echo $pageContents;
     <?php
 	$name ="PHP Store";
 	$credit =1000;
+	
+	$amount=800;
+	$taxRate=0.0825;
+	$addedTax = $amount*$taxRate;
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
     echo "<h1>Welcome to ".$name."!</h1>";
 	echo "<h2>You have $".$credit." in your wallet.</h2>";
+	echo $addedTax;
 	
 		$products['Computer']=750;
 		$products['Car']=15000;
@@ -559,6 +564,14 @@ echo $pageContents;
 				echo "<p>".$key."</p>";
 		}
 		}
+		
+		  function tax_calc($amount,$tax){
+			$calculate_tax = $amount*$tax;
+			$amount = round($amount+$calculate_tax,2);
+			return $amount;
+		  }
+		  
+		  echo tax_calc(750,0.233);
     ?>
   </body>
 </html>
