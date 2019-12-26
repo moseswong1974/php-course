d47a4d035838b64330795db85916c32fa8921cee
Wong <cheewai.wong@thermofisher.com>
2019-12-26 08:39:08 +0800

    add variables

--- index.php
+++ index.php
@@ -537,9 +537,12 @@ echo $pageContents;
   </head>
   <body>
     <?php
+	$name ="PHP Store";
+	$credit =1000;
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome! To PHP</h1>";
+    echo "<h1>Welcome to ".$name."!</h1>";
+	echo "<h2>You have $".$credit."</h2>";
     ?>
   </body>
 </html>
